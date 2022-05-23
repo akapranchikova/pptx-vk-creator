c4accf9f09d9442ac95369e46d9ff7a96bab1226
online-advertising-local <online-advertising-local@wildberries.ru>
2022-05-23 22:03:55 +0300

    refactoring

--- main.js
+++ main.js
@@ -3,19 +3,11 @@ const puppeteer = require("puppeteer");
 const pptxgen = require("pptxgenjs");
 
 
-
 (async () => {
     let pptx = new pptxgen();
     pptx.defineSlideMaster({
         title: "MASTER_SLIDE",
         background: { color: "FFFFFF" },
-        objects: [
-            // { line: { x: 3.5, y: 1.0, w: 6.0, line: { color: "0088CC", width: 5 } } },
-            // { rect: { x: 0.0, y: 5.3, w: "100%", h: 0.75, fill: { color: "F1F1F1" } } },
-            // { text: { text: "Status Report", options: { x: 3.0, y: 5.3, w: 5.5, h: 0.75 } } },
-            // { image: { x: 0.31, y: 0.3, w: 2.8125/3, h: 1.9063/3, path: "logo.png" } },
-        ],
-        // slideNumber: { x: 0.3, y: "90%" },
     });
 
     const yTop = 25;
@@ -34,7 +26,6 @@ const pptxgen = require("pptxgenjs");
             if (i !== 0 || j !== 0) {
                 await page.waitForNavigation()
             }
-
             console.log(`Slide - ${j + 1}, image - ${i + 1}`);
             const element2 = await page.$('#wk_content');
             await element2.screenshot({path: `screens/screenshot${i}${j}.jpeg`})
@@ -66,32 +57,6 @@ const pptxgen = require("pptxgenjs");
             await page.click('#wk_left_arrow_bg');
         }
     }
-
     pptx.writeFile();
     await browser.close()
 })()
-
-
-function createNewSlide(pptx) {
-    let slide = pptx.addSlide({ masterName: "MASTER_SLIDE" });
-
-    console.log(pptx._presLayout)
-    const yTop = '30%'
-    const width = '15%';
-    // TOP: 1
-    slide.addText("Sizing: Orig `w:6, h:2.7`", { x: 0.5, y: 0.6, w: 3.0, h: 0.3, color: COLOR_BLUE });
-    slide.addImage({ path: '..'+image, x: '3%', y: yTop, w: width, h: 46.427083333/6, sizing: {type: "crop", w: width, h: 2, y: 0}});
-
-    // TOP: 2
-    slide.addText("Sizing: `contain, w:3`", { x: 0.6, y: 4.25, w: 3.0, h: 0.3, color: COLOR_BLUE });
-    slide.addImage({ path: '..'+image, x: '19%', y: yTop, w: width, h: 46.427083333/6, sizing: { type: "crop", w: width, h: '20%', y: 0} });
-
-    slide.addImage({ path: '..'+image, x: '35%%', y: yTop, w: width, h: 46.427083333/6, sizing: { type: "crop", w: width, h: '20%', y: 0} });
-
-    // TOP: 2
-    // slide.addText("Sizing: `contain, w:3`", { x: 0.6, y: 4.25, w: 3.0, h: 0.3, color: COLOR_BLUE });
-    slide.addImage({ path: '..'+image, x: '51%', y: yTop, w: width, h: 4457, sizing: { type: "crop", w: width, h: '20%', y: 0} });
-    slide.addImage({ path: '..'+image, x: '67%', y: yTop, w: width, h: 4457, sizing: { type: "crop", w: width, h: '20%', y: 0} });
-    slide.addImage({ path: '..'+image, x: '83%', y: yTop, w: width, h: 4457, sizing: { type: "crop", w: width, h: '20%', y: 0} });
-
-}
